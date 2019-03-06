2f22dfb26b97162ca285efd042a2dff72c0bed8b
pranavdaa <pranavmaheshwari4321@gmail.com>
2019-03-07 02:06:08 +0530

    new try

---
+++ Desktop/webdev/node-restful-api-tutorial/app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+
+  mongoose.connect('mongodb://newuser:prabat123@ds161345.mlab.com:61345/acemedia',function(err){
+    if(err){
+    console.log('Some problem with the connection ' +err)
+    }
+    else {
+    console.log('The Mongoose connection is ready')
+    }
+
+})
+mongoose.Promise = global.Promise;
+app.use('/uploads',express.static('uploads')); //make the upload folder statically avalable to all
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
